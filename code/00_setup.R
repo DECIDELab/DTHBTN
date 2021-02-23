ac9453b2af016ebd241ba39befedc3a106dbaa26
lampk <phungkhanhlam@gmail.com>
2021-02-24 03:28:24 +0700

    add current stuffs

--- code/00_setup.R
+++ code/00_setup.R
@@ -9,3 +9,13 @@ wflow_publish(c("analysis/index.Rmd", "analysis/overview.Rmd", "analysis/disease
 wflow_status()
 wflow_use_github("decidelab")
 wflow_git_push()
+
+
+# 2021-02-24 --------------------------------------------------------------
+
+
+wflow_publish(c("analysis/overview.Rmd", "analysis/disease.Rmd", "analysis/slide.Rmd",
+                "analysis/DTHBTN_BenhTruyenNhiem.Rmd", "analysis/DTHBTN_BenhDuongTieuHoa.Rmd",
+                "analysis/DTHBTN_BenhLayQuaVecto.Rmd"),
+              "Add current stuffs")
+
